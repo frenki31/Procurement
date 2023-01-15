772b5b0e086c046fdcc89e88da70e7c8f5998671
Frenki Muca <frenki@muca.com>
2023-01-15 20:35:54 +0100

    sections added

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,7 @@
-
+const toTop = document.querySelector(".to-top");
+window.addEventListener("scroll", () => {
+    if(window.pageYOffset > 150)
+        toTop.classList.add("active");
+    else
+        toTop.classList.remove("active");
+})
