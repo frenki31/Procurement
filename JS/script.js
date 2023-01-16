0628e5bf2a8c1ac4d05bbbc159802adc32228965
Frenki Muca <frenki@muca.com>
2023-01-16 20:28:23 +0100

    login page added

--- JS/script.js
+++ JS/script.js
@@ -4,4 +4,20 @@ window.addEventListener("scroll", () => {
         toTop.classList.add("active");
     else
         toTop.classList.remove("active");
-})
+});
+
+function sendEmail(){
+    Email.send({
+      Host : "smtp.gmail.com",
+      Username : "tt7249031@gmail.com",
+      Password : "33DE160DB3614C87554CA53B3A2E0FB70584",
+      To : "frenki.muca@gmail.com",
+      From : document.getElementById("email").value,
+      Subject : "New Enquiry",
+      Body : "Name: " + document.getElementById("name").value
+      + "<br> Email: " + document.getElementById("email").value
+      + "<br> Message: " + document.getElementById("message").value
+    }).then(
+      message => alert("Email Sent Succesfully")
+    );
+  }
